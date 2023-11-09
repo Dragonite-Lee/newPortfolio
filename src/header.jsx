1a4ae092e4b9d737a529880cb59fb0e1aaf28c7e
Dragonite-Lee <ross991222@gmail.com>
2023-11-10 03:20:21 +0900

    재수정

--- src/header.jsx
+++ src/header.jsx
@@ -24,13 +24,13 @@ export default function Header() {
     return (
         <div className="NanumSquareNeo-Variable w-full shadow-2xl px-12 fixed z-40 bg-cover bg-[url('/src/image/navi.png')] min-w-[768px] font-bold">
                 <div className="h-16 flex gap-6 text-white justify-center items-center">
-                    <div ref={e => pillsRef.current[0] = e}>
+                    <div ref={e => textRef.current[0] = e}>
                         Who am I
                     </div>
-                    <div ref={e => pillsRef.current[1] = e}>
+                    <div ref={e => textRef.current[1] = e}>
                         Skills & Certificate
                     </div>
-                    <div ref={e => pillsRef.current[2] = e}>
+                    <div ref={e => textRef.current[2] = e}>
                         Projects
                     </div>
                 </div>
